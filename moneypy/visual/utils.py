9b805cad9e79180773673dd5f5263fe29a23a345
DanielPDWalker <danielpdwalker@gmail.com>
2018-03-27 18:27:24 +0100

    Changed a dot to a comma. Think I've fixed the line ending issue

--- moneypy/visual/utils.py
+++ moneypy/visual/utils.py
@@ -13,7 +13,8 @@ def min_max_date_format(df_list):
     return new_list
 
 def max_date_range(date_tuple_list):
-    min_date, max_date = date_tuple_list[0][0], date_tuple_list[0][0]
+    min_date = pd.to_datetime('1979-10-12 00:00:00')
+    max_date = pd.to_datetime('1979-10-12 00:00:00')
     for t in date_tuple_list:
         for i in t:
             if i < min_date:
@@ -23,7 +24,7 @@ def max_date_range(date_tuple_list):
     return min_date, max_date
 
 def format_date_to_string(date_tuple, date_format="yymmdd")
-    min_date, max_date = date_tuple[0]. date_tuple[1]
+    min_date, max_date = date_tuple[0], date_tuple[1]
     if date_format.lower() == "yymmdd":
         min_date, max_date = min_date.strftime('%y/%m/%d'), max_date.strftime('%y/%m/%d')
     elif date_format.lower() == "yymm":
