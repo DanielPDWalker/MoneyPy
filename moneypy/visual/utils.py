948d10263d09768947e4b206e1ae9794787e5442
DanielPDWalker <danielpdwalker@gmail.com>
2018-03-30 13:50:08 +0100

    Pep8 adherence

--- moneypy/visual/utils.py
+++ moneypy/visual/utils.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
+
 def min_max_date(df_list):
+
     new_list = []
     for df in df_list:
-        new_list.append((df['Date'].min(),df['Date'].max()))
+        new_list.append((df['Date'].min(), df['Date'].max()))
     return new_list
+
 
 def min_max_date_format(df_list):
     new_list = []
     for df in df_list:
-        new_list.append((df['Date'].min().strftime('%y/%m/%d'),df['Date'].max().strftime('%y/%m/%d')))
+        new_list.append((df['Date'].min().strftime('%y/%m/%d'),
+                         df['Date'].max().strftime('%y/%m/%d')))
     return new_list
+
 
 def max_date_range(date_tuple_list):
     min_date = pd.to_datetime('1979-10-12 00:00:00')
@@ -23,12 +28,13 @@ def max_date_range(date_tuple_list):
                 max_date = i
     return min_date, max_date
 
-def format_date_to_string(date_tuple, date_format="yymmdd")
+
+def format_date_to_string(date_tuple, date_format="yymmdd"):
     min_date, max_date = date_tuple[0], date_tuple[1]
     if date_format.lower() == "yymmdd":
-        min_date, max_date = min_date.strftime('%y/%m/%d'), max_date.strftime('%y/%m/%d')
+        min_date = min_date.strftime('%y/%m/%d')
+        max_date = max_date.strftime('%y/%m/%d')
     elif date_format.lower() == "yymm":
-        min_date, max_date = min_date.strftime('%y/%m'), max_date.strftime('%y/%m')
+        min_date = min_date.strftime('%y/%m')
+        max_date = max_date.strftime('%y/%m')
     return min_date, max_date
-
-
--- moneypy/visual/visulisation.py
+++ moneypy/visual/visulisation.py
@@ -3,33 +3,37 @@ import plotly.graph_objs as go
 import colorlover as cl
 import utils as utils
 
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.offline import iplot
 
 
 def plot_line_chart(df_list, colors=None):
     colors = colors or cl.scales['10']['div']
-        
     data = []
+
     date_range_tuple = utils.min_max_date(df_list)
     max_range_tuple = utils.max_date_range(date_range_tuple)
     dates_as_string_tuple = utils.format_date_to_string(max_range_tuple)
     date_range_tuple = utils.min_max_date_format(df_list)
-    
+
     for index, df in enumerate(df_list):
         line_chart = go.Scatter(
-            x = df['Date'],
-            y = df['Amount'],
+            x=df['Date'],
+            y=df['Amount'],
             hoverinfo='y+x',
-            name = df.name + " ({} - {})".format(str(date_range_tuple[index][0]),str(date_range_tuple[index][1]),
-            line = dict(
-                width = 2,
-                dash = 'dot'))
-        
+            name=df.name + " ({} - {})".format(
+                str(date_range_tuple[index][0]),
+                str(date_range_tuple[index][1])),
+            line=dict(
+                width=2,
+                dash='dot'))
+
         data.append(line_chart)
-        
-    layout=dict(
-        title = '{} to {} Line Chart'.format(dates_as_string_tuple[0], dates_as_string_tuple[1]),
-        hoverdistance = 1,
+
+    layout = dict(
+        title='{} to {} Line Chart'.format(
+            dates_as_string_tuple[0],
+            dates_as_string_tuple[1]),
+        hoverdistance=1,
         xaxis=dict(
             tickformat='%b %d',
             tickmode="auto",
@@ -37,33 +41,35 @@ def plot_line_chart(df_list, colors=None):
             ticks='outside',
             ticklen=4,
             tickwidth=2,
-            title = 'Date'),
-        yaxis = dict(title = 'Amount'))
-    
+            title='Date'),
+        yaxis=dict(title='Amount'))
+
     fig = dict(data=data, layout=layout)
     iplot(fig)
 
 
 def plot_grouped_bar_chart(df_list, colors=None):
     colors = colors or cl.scales['10']['div']
-        
+
     data = []
     date_range_tuple = utils.min_max_date(df_list)
     max_range_tuple = utils.max_date_range(date_range_tuple)
-    dates_as_string_tuple = utils.format_date_to_string(max_range_tuple, "yymm")        
-    
+    dates_as_string_tuple = utils.format_date_to_string(max_range_tuple,
+                                                        "yymm")
+
     for df in df_list:
         bar_chart = go.Bar(
             x=df['Date'],
             y=df['Amount'],
-            name = df.name,
+            name=df.name,
             opacity=0.75)
-    
+
         data.append(bar_chart)
     data = data
     layout = go.Layout(
         barmode='group',
-        title = '{} to {} Income & Outgoings Comparison'.format(dates_as_string_tuple[0],dates_as_string_tuple[1]),
+        title='{} to {} Income & Outgoings Comparison'.format(
+            dates_as_string_tuple[0], dates_as_string_tuple[1]),
         xaxis=dict(
             tickformat='%B',
             autotick=True,
@@ -71,26 +77,29 @@ def plot_grouped_bar_chart(df_list, colors=None):
             ticks='outside',
             ticklen=6,
             tickwidth=2,))
-    
+
     fig = go.Figure(data=data, layout=layout)
     iplot(fig)
 
 
 def plot_pie_chart(df_list, colors=None):
     colors = colors or cl.scales['10']['div']
-        
+
     data = []
 
     for df in df_list:
-        pie_chart = go.Pie(labels = df['Description'], values = df['Amount'], 
-                   hoverinfo='label+percent', 
-                   textinfo='value', 
-                   textfont=dict(size=18), marker=dict(colors=colors, line=dict(color="#000000", width=1)))
+        pie_chart = go.Pie(
+            labels=df['Description'],
+            values=df['Amount'],
+            hoverinfo='label+percent',
+            textinfo='value',
+            textfont=dict(size=18),
+            marker=dict(colors=colors, line=dict(color="#000000", width=1)))
         data.append(pie_chart)
-        
+
     data = data
     layout = go.Layout(
-        title = '{} Pie Chart'.format(df.name))
-    
+        title='{} Pie Chart'.format(df.name))
+
     fig = go.Figure(data=[pie_chart], layout=layout)
-    iplot(fig)     
+    iplot(fig)
