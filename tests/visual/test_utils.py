3a7e2d5dc75dc2d1cb762ab15c57bf096a2fc39a
DanielPDWalker <danielpdwalker@gmail.com>
2018-04-14 17:43:02 +0100

    Basics of unittesting and one test

---
+++ tests/visual/test_utils.py
@@ -0,0 +1,51 @@
+import unittest
+import random
+import pandas as pd
+from moneypy.visual import utils
+
+
+# Set up two empty dataframes.
+df_one = pd.DataFrame()
+df_two = pd.DataFrame()
+
+# Generate a random year between 2000 and 2016.
+random_date = 2000 + random.randrange(0, 16)
+
+# Create a date column and add 3 dates to it, based on the random_date above.
+df_one['Date'] = pd.date_range(start=str(random_date),
+                               periods=3,
+                               freq=str(random.randrange(10, 100)) + 'D')
+
+random_date = 2000 + random.randrange(0, 16)
+df_two['Date'] = pd.date_range(start=str(random_date),
+                               periods=3,
+                               freq=str(random.randrange(10, 100)) + 'D')
+
+# Saves the min and max dates of each dataframe, to test against.
+df_one_start_date = df_one['Date'].min()
+df_one_end_date = df_one['Date'].max()
+
+df_two_start_date = df_two['Date'].min()
+df_two_end_date = df_two['Date'].max()
+
+# Randomize the order of the dates.
+df_one = df_one.sample(frac=1)
+df_two = df_two.sample(frac=1)
+
+# Create the list of dataframes to pass to functions.
+df_list = [df_one, df_two]
+
+
+class TestUtils(unittest.TestCase):
+
+    """Unittest for utils.py"""
+
+    def test_min_max_date(self):
+        self.assertEqual(utils.min_max_date(df_list),
+            [(df_one_start_date, df_one_end_date),
+            (df_two_start_date, df_two_end_date)])
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
